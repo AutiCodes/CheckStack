d61509859b8a6dcb8f6c901beccd72663aeae0b9
AutiCodes <prive@auticodes.nl>
2024-11-26 21:10:41 +0100

    Added email monitor frontend monitor_create

--- Modules/Monitor/resources/views/monitor_create.blade.php
+++ Modules/Monitor/resources/views/monitor_create.blade.php
@@ -12,9 +12,12 @@
         Monitor type
       </p>
       <div class="inline-block relative w-64">
-        <select name="monitor_type" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+        <select name="monitor_type" id="monitor_type" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
           <option value=1>
             HTTP(S)
+          </option>
+          <option value=2>
+            Email services
           </option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
@@ -22,16 +25,88 @@
         </div>
       </div>
 
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
-        Name
-      </label>
-      <input name="name" class="appearance-none block w-full bg-gray-200 border border-red-500 rounded py-3 px-4 mb-3 leading-tight  focus:bg-white" id="url" type="text" placeholder="">
+      <div id="https" style="display: none;">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+          Name
+        </label>
+        <input name="name" class="appearance-none block w-full bg-gray-200 border border-red-500 rounded py-3 px-4 mb-3 leading-tight  focus:bg-white" id="url" type="text" placeholder="">
 
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
-        URL
-      </label>
-      <input name="url" class="appearance-none block w-full bg-gray-200 border border-red-500 rounded py-3 px-4 mb-3 leading-tight  focus:bg-white" id="url" type="text" placeholder="">
+        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="name">
+          URL
+        </label>
+        <input name="url" class="appearance-none block w-full bg-gray-200 border border-red-500 rounded py-3 px-4 mb-3 leading-tight  focus:bg-white" id="url" type="text" placeholder="">
+      </div>
 
+      <div id="email_services" style="display: none;">
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            General name
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email_name" name="email_name" type="text" placeholder="General name">
+        </div>
+
+        <hr>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email_internal">
+            Internal address
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email_internal" name="email_internal" type="email" placeholder="Internal email">
+
+          <label class="block text-gray-700 text-sm font-bold mb-2 " for="password">
+            Password
+          </label>
+          <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password_internal" name="internal_password" type="password" placeholder="******************">
+        </div>
+
+        <hr>
+
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            External address (Outlook - Gmail)
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email_external" name="email_external" type="email" placeholder="External email">
+
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password_external" name="password_external" type="password" placeholder="******************">
+        </div>
+
+        <hr>
+
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            Email forward recieve address
+          </label>
+          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email_forward" name="email_forward" type="email" placeholder="Forward recieve email">
+
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+            Password
+          </label>
+          <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password_forward" name="password_forward" type="password" placeholder="******************">
+        </div>
+
+        <hr>
+      </div>
+
+      <script>
+        document.getElementById('https').style.display = 'block';
+
+        document.getElementById('monitor_type').addEventListener('change', (event) => {
+          console.log(event.target.value);
+          if (event.target.value == 1) {
+            document.getElementById('https').style.display = 'block';
+          } else {
+            document.getElementById('https').style.display = 'none';
+          }
+
+          if (event.target.value == 2) {
+            document.getElementById('email_services').style.display = 'block';
+          } else {
+            document.getElementById('email_services').style.display = 'none';
+          }
+        })
+      </script>
       <p>
         Monitor interval
       </p>
